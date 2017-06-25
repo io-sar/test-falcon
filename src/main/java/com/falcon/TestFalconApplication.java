f8daa51103c901473fe2fa120e78bbe53445cdf5
io-sar <sarantidis.j@gmail.com>
2017-06-25 11:30:33 +0200

    Initial commit

---
+++ src/main/java/com/falcon/TestFalconApplication.java
@@ -0,0 +1,12 @@
+package com.falcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFalconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFalconApplication.class, args);
+	}
+}
